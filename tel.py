459f2d656256da6dc833b11ca4c5d96987a710e0
jomasicke <marijanamilankov86@gmail.com>
2021-11-14 20:08:03 +0100

    Dodala sam tel

---
+++ tel.py
@@ -0,0 +1,12 @@
+model = ("n-35")
+proizvodjac = ("Huawei")
+cena = 200
+slika = "https://www.google.com/search?q=huawei+telefoni&sxsrf=AOaemvLdnjehHFIbpfl4IwiNzCpHHcNMoA:1636580506187&source=lnms&tbm=isch&sa=X&sqi=2&ved=2ahUKEwj5md694Y70AhVWQ_EDHfGkDAYQ_AUoAXoECAEQAw&biw=1536&bih=754&dpr=1.25#imgrc=Y7oVI9EAxrhgIM"
+
+print("Koji mobilni telefon imas?")
+print("proizvodjac:")
+print(proizvodjac)
+print("cena:")
+print(cena)
+print("slika")
+print(slika)
